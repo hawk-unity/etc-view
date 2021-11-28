007663b7bd31a8536b9d0d538ff80b2a39b04462
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-11-28 09:53:45 -0500

    Update logred.py

--- logred.py
+++ logred.py
@@ -1,5 +1,22 @@
-pswd = file( "/etc/passwd", "r" )
+import os
+print("""
+--------------------------------------------
+instagram : faruk.developer
+web site: farukdeveloper.online
+youtube: HAWK DEFACER 
+--------------------------------------------
+         ETC PASSWD VİEW
+--------------------------------------------
+     
+""")
+pswd = open( "/etc/passwd", "r" )
 for aLine in pswd :
     fields= aLine.split( ":" )
     print (fields[0], fields[1])
 pswd.close()
+print("--------------------------------------------------------------", "\n" , "\n" , "\n")
+print()
+print()
+print()
+print("--------------------------------------------------------------")
+os.system("cat /etc/passwd ")
